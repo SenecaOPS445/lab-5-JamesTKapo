15ccd3b8224f02b6f030a85ef5a06e09b3b0df76
James Kapogiannis <73348066+JamesTKapo@users.noreply.github.com>
2024-07-12 14:54:35 -0400

    Add files via upload
    Large commit

---
+++ lab5a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Author ID: [seneca_id]
+
+def read_file_string(file_name):
+    f = open('data.txt', 'r')
+    file_name = f.read()
+    f.close() 
+    return file_name
+    # Takes file_name as string for a file name, returns its entire contents as a string
+
+def read_file_list(file_name):
+    f = open('data.txt', 'r')
+    list_of_lines = f.read().splitlines()
+    f.close()
+    return list_of_lines
+    # Takes a file_name as string for a file name, 
+    # return its entire contents as a list of lines without new-line characters
+
+if __name__ == '__main__':
+    file_name = 'data.txt'
+    print(read_file_string(file_name))
+    print(read_file_list(file_name))
